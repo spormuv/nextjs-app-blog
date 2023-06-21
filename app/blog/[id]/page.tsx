4e260fb310ba0fb61ac1c855b3621e4b13310280
Mike <spormuv@gmail.com>
2023-06-21 14:59:56 +0300

    feat: add error and preloader

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -6,16 +6,37 @@ type Props = {
   };
 };
 
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  return response.json();
+}
+
 export async function generateMetadata({
   params: { id },
 }: Props): Promise<Metadata> {
+  const post = await getData(id);
+
   return {
-    title: id,
+    title: post.title,
   };
 }
 
-const Post = ({ params: { id } }: Props) => {
-  return <h1>Post page {id}</h1>;
+const Post = async ({ params: { id } }: Props) => {
+  const post = await getData(id);
+
+  return (
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
+  );
 };
 
 export default Post;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,42 @@
 import { Metadata } from 'next';
+import Link from 'next/link';
+
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId?: number;
+};
+
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60,
+    },
+  });
+  if (!response.ok) throw new Error('Error while fetching posts');
+
+  return response.json();
+}
 
 export const metadata: Metadata = {
   title: 'Blog | Next App',
 };
 
-const Blog = () => {
-  return <h1>Blog page</h1>;
+const Blog = async () => {
+  const posts: Post[] = await getData();
+  return (
+    <>
+      <h1>Blog page</h1>
+      <ul>
+        {posts.map((post: Post) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Blog;
