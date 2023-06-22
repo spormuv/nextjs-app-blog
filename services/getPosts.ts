16acfedc94d092dcdd16290cb6cd2fe99116e0cf
Mike <spormuv@gmail.com>
2023-06-22 19:37:44 +0300

    feat: improve styles

--- services/getPosts.ts
+++ services/getPosts.ts
@@ -1,15 +1,17 @@
 export const getAllPosts = async () => {
-  // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const response = await fetch('/api/posts');
+  const response = await fetch(
+    'https://jsonplaceholder.typicode.com/posts?_limit=30'
+  );
+  // const response = await fetch('/api/posts');
   if (!response.ok) throw new Error('Error while fetching posts');
   return response.json();
 };
 
 export const getPostsBySearch = async (search: string) => {
-  // const response = await fetch(
-  //   `https://jsonplaceholder.typicode.com/posts?q=${search}`
-  // );
-  const response = await fetch(`/api/posts?q=${search}`);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?q=${search}`
+  );
+  // const response = await fetch(`/api/posts?q=${search}`);
   if (!response.ok) throw new Error('Error while fetching posts');
   return response.json();
 };
